77110726bf75197e5a6ea600ff8c915bad05dcb6
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2021-08-10 08:51:34 -0300

    chore: add jest & eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,52 +1,19 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true,
-    jasmine: true,
-    jest: true,
+    es2021: true,
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'prettier',
-    'react-app',
-  ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 2018,
+    ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: ['eslint-plugin-import-helpers', 'prettier', 'react', 'react-hooks'],
+  plugins: ['@typescript-eslint'],
   rules: {
-    // syntax
-    indent: ['error', 2, { SwitchCase: 1 }],
+    indent: ['error', 4, { SwitchCase: 1 }],
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
-    semi: ['error', 'always'],
-    // react
-    'react/forbid-prop-types': [0],
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-    'react/jsx-no-duplicate-props': [2, { ignoreCase: false }],
-    'react/jsx-props-no-spreading': [0],
-    'react/prop-types': [1],
-    // react-hooks
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
-    // import-helpers
-    'import-helpers/order-imports': [
-      'warn',
-      {
-        newlinesBetween: 'always',
-        groups: ['/^react/', 'module', ['parent', 'sibling', 'index']],
-        alphabetize: { order: 'asc', ignoreCase: true },
-      },
-    ],
+    semi: ['error', 'never'],
   },
 };
