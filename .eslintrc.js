efb19d7225a6af1bcc29438e1eb3e878c3a3baa2
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2021-08-11 17:49:58 -0300

    chore: add ts-node-dev

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,24 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint'],
-  rules: {
-    indent: ['error', 4, { SwitchCase: 1 }],
-    'linebreak-style': ['error', 'unix'],
-    quotes: ['error', 'single'],
-    semi: ['error', 'never'],
-  },
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	globals: {
+		exports: true,
+		module: true,
+		require: true,
+	},
+	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 12,
+		sourceType: 'module',
+	},
+	plugins: ['@typescript-eslint'],
+	rules: {
+		indent: ['error', 'tab', { SwitchCase: 1 }],
+		'linebreak-style': ['error', 'unix'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'always'],
+	},
 };
---
+++ README.md
@@ -0,0 +1,4 @@
+# Devtalks Testes de Integração
+
+----------
+By [Victor B. Fiamoncini](https://github.com/Victor-Fiamoncini) ☕️
