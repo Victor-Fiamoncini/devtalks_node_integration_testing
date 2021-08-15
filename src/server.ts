15d0ed283f8775c7ee935bb06e124cb0815851c6
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2021-08-15 19:47:28 -0300

    refactor: add HOST var

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import app from '@main/App';
 
 const SERVER_PORT = 3333;
+const HOST = 'http://localhost';
 
 app.listen(SERVER_PORT, () => {
-	return console.log(`Server listening on: ${SERVER_PORT}`);
+	return console.log(`Server listening on: ${HOST}:${SERVER_PORT}`);
 });
